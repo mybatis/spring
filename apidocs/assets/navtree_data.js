6123375f7afb6fc0731511c9144108cb9cb6344b
Simo Tripodi <stripodi@adobe.com>
2013-02-11 20:25:59 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,21 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.spring", "org/mybatis/spring/package-summary.html", [ [ "Description", "org/mybatis/spring/package-descr.html", null, "" ], [ "Classes", null, [ [ "MyBatisExceptionTranslator", "org/mybatis/spring/MyBatisExceptionTranslator.html", null, "" ], [ "SqlSessionFactoryBean", "org/mybatis/spring/SqlSessionFactoryBean.html", null, "" ], [ "SqlSessionHolder", "org/mybatis/spring/SqlSessionHolder.html", null, "" ], [ "SqlSessionTemplate", "org/mybatis/spring/SqlSessionTemplate.html", null, "" ], [ "SqlSessionUtils", "org/mybatis/spring/SqlSessionUtils.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "MyBatisSystemException", "org/mybatis/spring/MyBatisSystemException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.annotation", "org/mybatis/spring/annotation/package-summary.html", [ [ "Description", "org/mybatis/spring/annotation/package-descr.html", null, "" ], [ "Classes", null, [ [ "MapperScannerRegistrar", "org/mybatis/spring/annotation/MapperScannerRegistrar.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "MapperScan", "org/mybatis/spring/annotation/MapperScan.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.batch", "org/mybatis/spring/batch/package-summary.html", [ [ "Description", "org/mybatis/spring/batch/package-descr.html", null, "" ], [ "Classes", null, [ [ "MyBatisBatchItemWriter", "org/mybatis/spring/batch/MyBatisBatchItemWriter.html", null, "" ], [ "MyBatisPagingItemReader", "org/mybatis/spring/batch/MyBatisPagingItemReader.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.config", "org/mybatis/spring/config/package-summary.html", [ [ "Description", "org/mybatis/spring/config/package-descr.html", null, "" ], [ "Classes", null, [ [ "MapperScannerBeanDefinitionParser", "org/mybatis/spring/config/MapperScannerBeanDefinitionParser.html", null, "" ], [ "NamespaceHandler", "org/mybatis/spring/config/NamespaceHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.mapper", "org/mybatis/spring/mapper/package-summary.html", [ [ "Description", "org/mybatis/spring/mapper/package-descr.html", null, "" ], [ "Classes", null, [ [ "ClassPathMapperScanner", "org/mybatis/spring/mapper/ClassPathMapperScanner.html", null, "" ], [ "MapperFactoryBean", "org/mybatis/spring/mapper/MapperFactoryBean.html", null, "" ], [ "MapperScannerConfigurer", "org/mybatis/spring/mapper/MapperScannerConfigurer.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.support", "org/mybatis/spring/support/package-summary.html", [ [ "Description", "org/mybatis/spring/support/package-descr.html", null, "" ], [ "Classes", null, [ [ "SqlSessionDaoSupport", "org/mybatis/spring/support/SqlSessionDaoSupport.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mybatis.spring.transaction", "org/mybatis/spring/transaction/package-summary.html", [ [ "Description", "org/mybatis/spring/transaction/package-descr.html", null, "" ], [ "Classes", null, [ [ "SpringManagedTransaction", "org/mybatis/spring/transaction/SpringManagedTransaction.html", null, "" ], [ "SpringManagedTransactionFactory", "org/mybatis/spring/transaction/SpringManagedTransactionFactory.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
