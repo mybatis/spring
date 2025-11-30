42192fdca8bbad47e7d64e6172c0d91cf96745a9
Jeremy Landis <jeremylandis@hotmail.com>
2025-11-30 13:58:20 -0500

    [javadocs] Fix javadoc on test side
    did not address any warnings as a lot of them and those only affect us

--- src/test/java/org/mybatis/spring/filter/xml/XmlScanFilterTest.java
+++ src/test/java/org/mybatis/spring/filter/xml/XmlScanFilterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 the original author or authors.
+ * Copyright 2010-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * test the function of excludeFilters in <mybatis:scan/>
+ * test the function of excludeFilters in <code><mybatis:scan/></code>.
  */
 public class XmlScanFilterTest {
 
